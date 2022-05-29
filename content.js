7cb74f2d007e776bb201576337ca1633f3064348
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2022-05-29 21:21:45 +0200

    delay .5 sec

--- content.js
+++ content.js
@@ -1,12 +1,14 @@
-ids = ['#gdpr-consent-tool-wrapper','#qc-cmp2-container','#cookie-banner']
-ids.forEach (id => {
-  try {
-    popup = document.querySelector(id);
-    if (popup) {
-      popup.remove();
-      console.log(id + ' removed');
-    }    
-  } catch (e) {
-    console.error(e,id);
-  }  
-}); 
+setTimeout(()=>{
+  ids = ['#gdpr-consent-tool-wrapper','#qc-cmp2-container','#cookie-banner']
+  ids.forEach (id => {
+    try {
+      popup = document.querySelector(id);
+      if (popup) {
+        popup.remove();
+        console.log(id + ' removed');
+      }    
+    } catch (e) {
+      console.error(e,id);
+    }  
+  }); 
+},500);
