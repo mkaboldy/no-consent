7d6179f3f82fa4076cd7f32ec26573ec43988d8c
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2022-06-07 23:11:46 +0200

    more popup  selectors

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Chrome extension to prevent CMP consent box from popping up
 
 ## install
-- chrome://extensions
+- Open <a href="chrome://extensions" target="_blank">chrome://extensions</a>
 - Enable Developer Mode by clicking the toggle switch next to Developer mode
 - Click the Load unpacked button and select the extension directory
--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 setTimeout(()=>{
   // frequent cookie consent popups
-  const selectors = ['#alert18', '.truste_box_overlay', '.truste_overlay', '.fc-consent-root','.newsletter-layer','#gdpr-consent-tool-wrapper','#qc-cmp2-container','#cookie-banner']
+  const selectors = ['#cookie_alert','.js-consent-banner', '#alert18', '.truste_box_overlay', '.truste_overlay', '.fc-consent-root','.newsletter-layer','#gdpr-consent-tool-wrapper','#qc-cmp2-container','#cookie-banner']
   selectors.forEach (selector => {
     try {
       let popup = document.querySelector(selector);
@@ -18,5 +18,14 @@ setTimeout(()=>{
     popup.remove();
     document.querySelector('body').classList.remove('sf-is-modal-overflow-hidden');
     console.log('gdpr-consent-modal removed');
+  }
+  // port.hu
+  if (location.href.includes('port.hu')) {
+    document.querySelector('body').classList.remove('no-touch');
+    document.querySelector('body').style = '';
+  }
+  // babanet.hu
+  if (location.href.includes('babanet.hu')) {
+    document.querySelector('body').style = '';
   }
 },500);
